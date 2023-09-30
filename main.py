4336211bc5dc014ff8b0508611c777752e270206
sardoramriyeyv <146510043+sardoramriyeyv@users.noreply.github.com>
2023-09-30 10:55:29 +0500

    python file

---
+++ main.py
@@ -0,0 +1,19 @@
+# import func
+
+
+
+# from func import*
+# # import func
+# function.ayirish(4,3)
+# function.add(2,3)
+
+
+
+# from func import add as b,ayir as a,mult,div
+import avto_info_mod
+
+
+import avto_info_mod # avto_info_mod faylini (modulini) chaqiramiz
+
+avto1 = avto_info_mod.avto_info("GM", "Malibu", "Qora", "avtomat", 2020,40000)
+avto_info_mod.info_print(avto1)
